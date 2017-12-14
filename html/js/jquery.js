b09c4b4cea62d2acf4509fb9f58cb65dab22d17f
novalyphe <novalyphe@gmail.com>
2017-12-14 11:09:05 +0200

    Pirmas projektas

---
+++ html/js/jquery.js
@@ -0,0 +1,23 @@
+$(document).ready (function () {
+
+
+
+    //surasti pagal id
+    var a = $ ('#para').html();
+    console.log (a);
+
+    //surasti visus p
+    var p = $("p");
+    for (var i=0; i<p.length; i++) console.log($(p[i].html());
+
+
+        // uzdeti css
+    $("#para").css({
+        'font-weight':'bold'
+        
+    });
+
+
+
+});
+
